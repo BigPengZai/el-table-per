ea794ebdd5e7fa58882349374f5e9af8fb8055df
pengppeng <pengpeng@zz-med.com>
2023-03-07 15:09:23 +0800

    完善组件逻辑

--- src/components/ElTablePer.vue
+++ src/components/ElTablePer.vue
@@ -1,6 +1,8 @@
 <template>
+  <!-- VirtualTable -->
+
   <el-table
-    v-my-scroll="{ dataList }"
+    v-my-scroll="{ dataList, itemHeight: 40 }"
     :data="dataList.slice(start, over)"
     style="width: 100%"
     height="300"
@@ -9,24 +11,42 @@
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
   </el-table>
+
+  <!-- 瞎几把加载数据 -->
+
+  <!-- <el-table :data="dataList" style="width: 100%" height="300">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table> -->
 </template>
 
 <script setup>
-import { onMounted, ref, watch, watchEffect, inject } from "vue";
-
-let dataList = ref([]);
+// import "element-plus/dist/index.css";
+// import { ElMessage } from "element-plus";
+import { onMounted, ref, inject, nextTick } from "vue";
 
 const { start, over } = inject("dataListOptions");
 console.log(start.value, over.value);
+
+let dataList = ref([]);
+let totalRows = 200;
 onMounted(() => {
+  const now = Date.now();
+
   setTimeout(() => {
-    for (let index = 1; index <= 200; index++) {
+    for (let index = 1; index <= totalRows; index++) {
       dataList.value.push({
         date: "2023-03-05-----" + index,
         name: "Tom",
         address: "No. 189, Grove St, Los Angeles",
       });
     }
+
+    console.log("mock fetch数据请求完成:", Date.now() - now);
+    setTimeout(() => {
+      console.log(`table渲染${totalRows}行时间：`, Date.now() - now - 1000);
+    }, 0);
   }, 1000);
 });
 </script>
--- src/main.js
+++ src/main.js
@@ -1,26 +1,42 @@
-import { createApp, watch, ref, computed, watchEffect } from "vue";
+import {
+  createApp,
+  watch,
+  ref,
+  computed,
+  watchEffect,
+  nextTick,
+  onMounted,
+} from "vue";
 import "./style.css";
 import App from "./App.vue";
 import throttle from "./utils/throttle";
 const app = createApp(App);
 
-const start = ref(0);
-const over = ref(15);
-
+let start = ref(0);
+let over = ref(0);
+let estimateItemHeight = 40;
 const padding = ref([]);
-
 let tableHeight = ref(300);
 let tableScrollTop = ref(0);
-let dataListA = [];
+let estimateDataList = [];
+// 偏移量 startOffset，滚动后将渲染区域 偏移到可视区域中
+let startOffset = ref(0);
+// over.value = start.value + tableHeight.value / estimateItemHeight;
+// console.log(over.value, 111);
 // 在绑定元素的父组件
 // 及他自己的所有子节点都挂载完成后调用
 app.directive("my-scroll", (el, binding, vnode, prevVnode) => {
-  const { dataList } = binding.value;
-  dataListA = dataList;
+  const { dataList, itemHeight = 40 } = binding.value;
+  estimateDataList = dataList;
+  estimateItemHeight = itemHeight;
+
+  if (over.value == 0) {
+    over.value = start.value + tableHeight.value / estimateItemHeight;
+    console.log(over.value);
+  }
   //   console.log(dataList.length);
   let target = el.querySelector(".el-scrollbar__wrap");
-  //事件是否用捕获事件，从外到里，true
-  //默认为false:使用冒泡事件，从里到外
+  let tabel = el.querySelector(".el-table__body");
   target.addEventListener("scroll", () => {
     // console.log(target.scrollTop, target.clientHeight, target.scrollHeight);
     // console.log("触底");
@@ -31,37 +47,74 @@ app.directive("my-scroll", (el, binding, vnode, prevVnode) => {
     //   over.value += 15;
     // }
 
-    setTimeout(() => {
-      let tabel = el.querySelector(".el-table__body");
-      tabel.style.paddingTop = padding[0] + "px";
-      tabel.style.paddingBottom = padding[1] + 50 + "px";
+    nextTick(() => {
+      // let tabel = el.querySelector(".el-table__body");
 
+      // console.log("设置padding");
+      // tabel.style.paddingTop = padding[0] + "px";
+      // tabel.style.paddingBottom = padding[1] + 50 + "px";
+
+      tabel.style.transform = getTransform.value;
+      // console.log("设置y轴的偏移量", scrollTop);
+      let scrollTop = target?.scrollTop || 0;
+      startOffset.value = scrollTop - (scrollTop % estimateItemHeight);
+    });
+
+    setTimeout(() => {
       tableHeight.value = target.clientHeight;
       tableScrollTop.value = target.scrollTop;
     }, 200);
   });
 });
+
+// start = computed(() => {
+//   return Math.max(Math.ceil(tableScrollTop.value / 40 - 5), 0);
+// });
+// over = computed(() => {
+//   let paddingBottom = (dataListA.length - over.value) * 40;
+//   let paddingTop = start.value * 40;
+
+//   padding[0] = paddingTop;
+//   padding[1] = paddingBottom;
+//   if (padding[0] > 7360) return;
+//   return Math.min(
+//     Math.ceil((tableScrollTop.value + tableHeight.value) / 40 + 5),
+//     200
+//   );
+// });
+
+// 偏移量对应的style
+const getTransform = computed(() => `translate3d(0,${startOffset.value}px,0)`);
+
+// start.value = computed(() => {
+//   console.log(tableScrollTop.value);
+//   return Math.max(Math.ceil(tableScrollTop.value / estimateItemHeight - 5), 0);
+// });
+// over.value = computed(() =>
+//   Math.min(
+//     Math.ceil(
+//       (tableScrollTop.value + tableHeight.value) / estimateItemHeight + 5
+//     ),
+//     estimateDataList.length
+//   )
+// );
+watch([tableHeight, tableScrollTop], () => {
+  start.value = Math.max(
+    Math.ceil(tableScrollTop.value / estimateItemHeight - 5),
+    0
+  );
+  over.value = Math.min(
+    Math.ceil(
+      (tableScrollTop.value + tableHeight.value) / estimateItemHeight + 5
+    ),
+    estimateDataList.length
+  );
+  // console.log("start:", start.value, "over", over.value);
+});
+
 app.provide("dataListOptions", {
   start,
   over,
-});
-computed(() => {});
-
-watch([tableHeight, tableScrollTop], () => {
-  start.value = Math.max(Math.ceil(tableScrollTop.value / 40 - 5), 0);
-
-  over.value = Math.min(
-    Math.ceil((tableScrollTop.value + tableHeight.value) / 40 + 5),
-    200
-  );
-
-  console.log(start.value, over.value);
-  let paddingBottom = (dataListA.length - over.value) * 40;
-  let paddingTop = start.value * 40;
-
-  padding[0] = paddingTop;
-  padding[1] = paddingBottom;
-  console.log(padding[0], padding[1]);
 });
 
 app.mount("#app");
